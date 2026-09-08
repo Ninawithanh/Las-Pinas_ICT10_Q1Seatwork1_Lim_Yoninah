2306927c17cfb5f2a5b50e78d100974cf3dc6bc6
Ninawithanh <lim.yoz@obmontessori.edu.ph>
2026-09-08 14:07:05 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#Lim, Tabaldo 
+studname = "Gilliah Anne Marie L. Tabaldo" #string
+studage = 15 # integer 
+studheight = 154.94 #Float
+studcountries = ["Germany", "Australia", "UK"] #list
+studtype = False #boolean 
+stud_like = {
+    "color": "red", 
+    "car_brand": "chevrolet", 
+    "shoe_size": 5.5, 
+    "best_friend": "River Maire Parreno Datiles"
+} 
+stud_fruits = {
+    "Mango",
+    "Banana",
+    "Blueberry",
+    "Watermelon", 
+    "Melon",
+}
+week_day = {
+    "Monday", 
+    "Tuesday", 
+    "Wednesday",
+    "Thursday", 
+    "Friday", 
+    "Saturday", 
+    "Sunday",
+}
